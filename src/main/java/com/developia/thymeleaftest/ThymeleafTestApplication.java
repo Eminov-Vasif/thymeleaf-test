529ea2a37e6747434a65949478c2de5814c04c58
Vasif Eminov <veminov@asm.local>
2020-05-15 16:59:23 +0400

    first commit

---
+++ src/main/java/com/developia/thymeleaftest/ThymeleafTestApplication.java
@@ -0,0 +1,26 @@
+package com.developia.thymeleaftest;
+
+import com.developia.thymeleaftest.model.Student;
+import com.developia.thymeleaftest.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTestApplication implements CommandLineRunner {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Student student = new Student();
+
+	}
+}
